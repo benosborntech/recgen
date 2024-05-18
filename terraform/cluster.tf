6cd57f53f9a9b357326c3ffe9e2adcab80bb0b11
Ben Osborn <benosborntech@gmail.com>
2024-05-18 10:50:13 +0000

    Update

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -1,11 +1,11 @@
 resource "digitalocean_kubernetes_cluster" "cluster" {
   name   = var.cluster_name
   region = var.region
-  version = "1.22.8-do.1"
+  version = "1.29.1-do.0"
 
   node_pool {
-    name       = "${var.cluster_name}-default-pool"
-    size       = var.cluster_min_nodes
+    name       = "${var.cluster_name}-worker-pool"
+    size       = var.cluster_node_size
     auto_scale = true
     min_nodes  = var.cluster_min_nodes
     max_nodes  = var.cluster_max_nodes
