4632bfd48c45a9bce6ae6905df37d3b7f45d3321
Ben Osborn <benosborntech@gmail.com>
2024-05-18 10:59:13 +0000

    Updated cluster

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -22,19 +22,19 @@ provider "digitalocean" {
 }
 
 provider "kubernetes" {
-    host    = digitalocean_kubernetes_cluster.default_cluster.endpoint
-    token   = digitalocean_kubernetes_cluster.default_cluster.kube_config[0].token
+    host    = digitalocean_kubernetes_cluster.cluster.endpoint
+    token   = digitalocean_kubernetes_cluster.cluster.kube_config[0].token
     cluster_ca_certificate = base64decode(
-        digitalocean_kubernetes_cluster.default_cluster.kube_config[0].cluster_ca_certificate
+        digitalocean_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate
     )
 }
 
 provider "helm" {
   kubernetes {
-    host  = digitalocean_kubernetes_cluster.default_cluster.endpoint
-    token = digitalocean_kubernetes_cluster.default_cluster.kube_config[0].token
+    host  = digitalocean_kubernetes_cluster.cluster.endpoint
+    token = digitalocean_kubernetes_cluster.cluster.kube_config[0].token
     cluster_ca_certificate = base64decode(
-        digitalocean_kubernetes_cluster.default_cluster.kube_config[0].cluster_ca_certificate
+        digitalocean_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate
     )
   }
 }
