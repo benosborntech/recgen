f88ac70e176597469df09679597b3f983fb4a54d
Ben Osborn <benosborntech@gmail.com>
2024-05-26 21:42:52 +0000

    Update state

--- terraform/project.tf
+++ terraform/project.tf
@@ -1,4 +1,4 @@
 resource "digitalocean_project" "project" {
   name      = var.digitalocean_project
-  resources = [digitalocean_kubernetes_cluster.cluster.urn]
+  resources = [digitalocean_kubernetes_cluster.cluster.urn, digitalocean_loadbalancer.ingress_load_balancer.urn]
 }
