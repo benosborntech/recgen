2e07fe8d6485ad162aaa18f37c003b5865ef2368
Ben Osborn <benosborntech@gmail.com>
2024-06-22 02:14:55 +0000

    Update

--- src/updaterecommendations/generatedata.py
+++ src/updaterecommendations/generatedata.py
@@ -19,13 +19,18 @@ def generate_data(cfg: Config, file: str, oai_client: openai.OpenAI) -> Data:
             response = oai_client.embeddings.create(input=description, model="text-embedding-ada-002")
             embeddings = response.data[0].embedding
 
-            data[key]["vector"] = np.array(embeddings, dtype=np.float32)
+            data[key]["vector"] = embeddings
 
-            cfg.get_logger().info(f"generating embeddings for '{key}'")
+            cfg.get_logger().info(f"generated embeddings for '{key}'")
 
         f.seek(0)
         json.dump(data, f)
 
     cfg.get_logger().info(f"loaded {len(data.keys())} items")
+
+    for key, value in data.items():
+        data[key]["vector"] = np.array(value["vector"], dtype=np.float32)
+
+    cfg.get_logger().info(f"converted to np array")
 
     return data
