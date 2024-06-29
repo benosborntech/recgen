896ce2da4bc58baa0443b08971a2dc8d292dbfac
Ben Osborn <benosborntech@gmail.com>
2024-06-29 02:28:30 +0000

    Added the rest of recommendation generation

--- src/updaterecommendations/handler.py
+++ src/updaterecommendations/handler.py
@@ -3,11 +3,11 @@ from redis.commands.search.query import Query
 
 from src.pyutils.config import Config
 from src.pyutils.keyconcat import key_concat
-from src.pyutils.constants import BF_PREFIX, DB_HASH_PREFIX, LOCK_TIMEOUT, LOCK_PREFIX, MAX_RECOMMENDATIONS, SET_PREFIX, VECTOR_INDEX, MAX_RESULTS
+from src.pyutils.constants import BF_PREFIX, DB_HASH_PREFIX, LOCK_TIMEOUT, LOCK_PREFIX, MAX_RECOMMENDATIONS, SET_PREFIX, VECTOR_INDEX, MAX_RESULTS, PAGE_SIZE
 from src.pyutils.model import Body
 
 
-def handle(cfg: Config, r_client: redis.Redis, body: Body) -> None:
+def handle(_: Config, r_client: redis.Redis, body: Body) -> None:
     lock = r_client.lock(key_concat(LOCK_PREFIX, body["userId"]), LOCK_TIMEOUT)
     lock.acquire(blocking=True)
 
@@ -20,19 +20,34 @@ def handle(cfg: Config, r_client: redis.Redis, body: Body) -> None:
         condition = True
         count = 0
 
-        query = (
-            Query(base_query)
-            .return_fields("id", "vector", "vector_score")
-            .sort_by("vector_score")
-            .paging(cursor, cursor + MAX_RESULTS)
-            .dialect(2)
-        )
-        params_dict = {"vector": vec}
+        while condition:
+            query = (
+                Query(base_query)
+                .return_fields("id", "vector", "vector_score")
+                .sort_by("vector_score")
+                .paging(cursor, cursor + MAX_RESULTS)
+                .dialect(2)
+            )
+            params_dict = {"vector": vec}
 
-        results = r_client.ft(VECTOR_INDEX).search(query, params_dict)
-        
-        for i, article in enumerate(results.docs):
-            cfg.get_logger().info(f"{i}. {article}")
+            results = r_client.ft(VECTOR_INDEX).search(query, params_dict)
+            
+            for article in results.docs:
+                exists = r_client.bf().exists(key_concat(BF_PREFIX, body["userId"]), body["itemId"])
+                if exists:
+                    continue
+
+                r_client.zadd(key_concat(SET_PREFIX, body["userId"]), {"score": article.vector_score, "member": body["itemId"]})
+
+            count = r_client.zcount(key_concat(SET_PREFIX, body["userId"]), 0, 1)
+
+            condition = count < MAX_RECOMMENDATIONS and len(results) == count
+            cursor += PAGE_SIZE
+
+        to_remove = count - MAX_RECOMMENDATIONS
+        if to_remove > 0:
+            r_client.zpopmin(key_concat(SET_PREFIX, body["userId"]), to_remove)
+
     else:
         r_client.bf().add(key_concat(BF_PREFIX, body["userId"]))
         r_client.zrem(key_concat(SET_PREFIX, body["userId"]), body["itemId"])
