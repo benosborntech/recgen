bf005eabc9a75a95d31dcf101d9a54681d0f5b07
Ben Osborn <benosborntech@gmail.com>
2024-06-29 04:39:28 +0000

    Fixed logs

--- src/updaterecommendations/handler.py
+++ src/updaterecommendations/handler.py
@@ -12,7 +12,7 @@ def handle(cfg: Config, r_client: redis.Redis, body: Body) -> None:
     lock = r_client.lock(k_lock, LOCK_TIMEOUT)
     lock.acquire(blocking=True)
 
-    cfg.get_logger(f"acquired lock {k_lock}")
+    cfg.get_logger().info(f"acquired lock {k_lock}")
 
     if body["positive"]:
         k_vec = key_concat(DB_HASH_PREFIX, body["itemId"])
@@ -37,24 +37,24 @@ def handle(cfg: Config, r_client: redis.Redis, body: Body) -> None:
             results = r_client.ft(VECTOR_INDEX).search(query, params_dict)
             
             for article in results.docs:
-                cfg.get_logger(f"found result for key {k_vec}: {article}")
+                cfg.get_logger().info(f"found result for key {k_vec}: {article}")
 
                 k_bf = key_concat(BF_PREFIX, body["userId"])
                 exists = r_client.bf().exists(k_bf, body["itemId"])
                 if exists:
-                    cfg.get_logger(f"skipping result as exists within filter {k_bf}")
+                    cfg.get_logger().info(f"skipping result as exists within filter {k_bf}")
 
                     continue
 
                 k_set = key_concat(SET_PREFIX, body["userId"])
                 r_client.zadd(k_set, {body["itemId"]: article.vector_score})
 
-                cfg.get_logger(f"adding result {body['itemId']} to set {k_set}")
+                cfg.get_logger().info(f"adding result {body['itemId']} to set {k_set}")
 
             k_count = key_concat(SET_PREFIX, body["userId"])
             count = r_client.zcount(k_count, 0, 1)
 
-            cfg.get_logger(f"count for {k_count} is {count}")
+            cfg.get_logger().info(f"count for {k_count} is {count}")
 
             condition = count < MAX_RECOMMENDATIONS and len(results.docs) == count
             cursor += PAGE_SIZE
@@ -64,17 +64,19 @@ def handle(cfg: Config, r_client: redis.Redis, body: Body) -> None:
             k_remove = key_concat(SET_PREFIX, body["userId"])
             r_client.zpopmin(k_remove, to_remove)
 
-            cfg.get_logger(f"removed for {k_remove} is {to_remove}")
+            cfg.get_logger().info(f"removed for {k_remove} is {to_remove}")
 
     else:
         k_bf = key_concat(BF_PREFIX, body["userId"])
         r_client.bf().add(k_bf)
 
-        cfg.get_logger(f"added key {k_bf} to filter")
+        cfg.get_logger().info(f"added key {k_bf} to filter")
 
         k_set = key_concat(SET_PREFIX, body["userId"])
         r_client.zrem(k_set, body["itemId"])
 
-        cfg.get_logger(f"added item {body['itemId']} to {k_set}")
+        cfg.get_logger().info(f"added item {body['itemId']} to {k_set}")
 
     lock.release()
+
+    cfg.get_logger().info(f"released lock {k_lock}")
