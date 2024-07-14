52616f46e8831cd5d5656400dca0b07334ccb8fe
Ben Osborn <benosborntech@gmail.com>
2024-07-14 02:36:05 +0000

    Update

--- src/pyutils/nn.py
+++ src/pyutils/nn.py
@@ -14,14 +14,14 @@ class RecommendationModel(nn.Module):
     def forward(self, user_id: str, item_emb: torch.Tensor) -> torch.Tensor:
         assert self.user_exists(user_id), "user does not exist"
 
-        user_emb = self.user_embeddings[user_id]
+        user_emb = self.user_embeddings.get(user_id)
         combined_emb = torch.cat((user_emb, item_emb), dim=1)
         output = torch.sigmoid(self.fc(combined_emb))
 
         return output
 
     def user_exists(self, user_id: str) -> bool:
-        return user_id in self.user_embeddings
+        return self.user_embeddings.get(user_id) != None
 
     def add_user(self, user_id: str) -> None:
         assert not self.user_exists(user_id), "user already exists"
